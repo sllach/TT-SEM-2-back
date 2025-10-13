bea16980ef0ace4126e0bd89c4ed70c27e24cfc0
sllach <sllach@utem.cl>
2025-10-13 17:07:13 -0300

    Delete api directory

--- api/models/colaborador_material.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type ColaboradorMaterial struct {
-	MaterialID uuid.UUID `gorm:"type:uuid;primaryKey" json:"material_id"`
-	UsuarioID  uint      `gorm:"primaryKey" json:"usuario_id"`
-}
