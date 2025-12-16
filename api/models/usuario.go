544053cdc2b0accdc48da67a8d136a8f0decb05f
sllach <sllach@utem.cl>
2025-12-16 17:33:32 -0300

    Update usuario.go

--- api/models/usuario.go
+++ api/models/usuario.go
@@ -7,16 +7,15 @@ import (
 )
 
 type Usuario struct {
-	// Quitamos gorm.Model para evitar que cree un ID uint automatico
 
-	GoogleID   string `gorm:"primaryKey;type:text" json:"google_id"` // <--- ESTO ES LA CLAVE AHORA
+	GoogleID   string `gorm:"primaryKey;type:text" json:"google_id"`
 	SupabaseID string `gorm:"type:text;unique" json:"supabase_id"`
 	Nombre     string `gorm:"size:255;not null" json:"nombre"`
 	Email      string `gorm:"size:255;not null;unique" json:"email"`
 	Rol        string `gorm:"default:'lector'" json:"rol"` // lector, colaborador, administrador
 
-	// Agregamos manualmente los campos de tiempo que quitamos al sacar gorm.Model
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
+
